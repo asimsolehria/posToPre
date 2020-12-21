f97ccf34bd1c8054371c828255f8174b9b7dbcf2
Asim Habib <de.asimhabib@gmail.com>
2020-12-21 23:36:22 +0500

    wrote method for converting a postfix espression to prefix

--- src/postopre/PosToPre.java
+++ src/postopre/PosToPre.java
@@ -1,11 +1,34 @@
 
 package postopre;
 
+import java.util.Stack;
+
 /**
  *
  * @author asim
  */
 public class PosToPre 
-{
+{    
     
+    public String posToPre(String exp)
+    {
+        Stack<String> stack= new Stack<>();
+        
+        for (int i = 0; i < exp.length(); i++) 
+        {
+            char c=exp.charAt(i);
+            
+            if(Character.isLetterOrDigit(c))
+                stack.push(c+"");
+            else
+            {
+                String opnd1=stack.pop();
+                String opnd2=stack.pop();
+                String prefix=c+opnd2+opnd1;
+                stack.push(prefix);
+            }
+            
+        }
+        return stack.pop();
+    }
 }
---
+++ src/postopre/main.java
@@ -0,0 +1,15 @@
+
+package postopre;
+
+/**
+ *
+ * @author asim
+ */
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        String postfix="ABC/-AK/L-*";
+        System.out.println(new PosToPre().posToPre(postfix));
+    }
+}
